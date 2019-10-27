30b8fbe1c25b6651f1d3c109fcbe4ce4bad3f43a
Tadas <vtadas25@gmail.com>
2019-10-27 12:37:41 +0200

    Implement is_slashable_attestation_data

--- helper-functions-2/src/predicates.rs
+++ helper-functions-2/src/predicates.rs
@@ -1,4 +1,4 @@
-use types::types::Validator;
+use types::types::{Validator, AttestationData};
 use types::primitives::Epoch;
 
 // Check if validator is active
@@ -11,12 +11,19 @@ pub fn is_slashable_validator(validator: &Validator, epoch: Epoch) -> bool {
     !validator.slashed && validator.activation_epoch <= epoch && epoch < validator.withdrawable_epoch
 }
 
+// Check if ``data_1`` and ``data_2`` are slashable according to Casper FFG rules.
+pub fn is_slashable_attestation_data(data_1: &AttestationData, data_2: &AttestationData) -> bool {
+    (data_1 != data_2 && data_1.target.epoch == data_2.target.epoch) ||
+    (data_1.source.epoch < data_2.source.epoch && data_2.target.epoch < data_1.target.epoch)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::u64::MAX as epoch_max;
     use bls::{PublicKey, SecretKey};
     use types::primitives::H256;
+    use types::types::{Checkpoint, Crosslink};
 
     fn default_validator() -> Validator {
         Validator{
@@ -28,6 +35,25 @@ mod tests {
             withdrawable_epoch: epoch_max,
             withdrawal_credentials: H256([0; 32]),
             pubkey: PublicKey::from_secret_key(&SecretKey::random())
+        }
+    }
+
+    fn default_crosslink() -> Crosslink {
+        Crosslink {
+            shard: 0,
+            parent_root: H256([0; 32]),
+            start_epoch: 0,
+            end_epoch: 1,
+            data_root: H256([0; 32])
+        }
+    }
+
+    fn default_attestation_data() -> AttestationData {
+        AttestationData {
+            beacon_block_root: H256([0; 32]),
+            source: Checkpoint{ epoch: 0, root: H256([0; 32]) },
+            target: Checkpoint{ epoch: 0, root: H256([0; 32]) },
+            crosslink: default_crosslink()
         }
     }
 
@@ -94,6 +120,49 @@ mod tests {
         let epoch: u64 = 10;
 
         assert!(is_slashable_validator(&validator, epoch));
+    }
+
+    #[test]
+    fn test_double_vote_attestation_data() {
+        let mut data_1 = default_attestation_data();
+        let data_2 = default_attestation_data();
+        data_1.target.root = H256([1; 32]);
+
+        assert!( is_slashable_attestation_data(&data_1, &data_2) );
+    }
+
+    #[test]
+    fn test_equal_attestation_data() {
+        let data_1 = default_attestation_data();
+        let data_2 = default_attestation_data();
+
+        assert!( !is_slashable_attestation_data(&data_1, &data_2) );
+    }
+
+    #[test]
+    fn test_surround_vote_attestation_data() {
+        let mut data_1 = default_attestation_data();
+        let mut data_2 = default_attestation_data();
+        data_1.source.epoch = 0;
+        data_2.source.epoch = 1;
+        data_1.target.epoch = 4;
+        data_2.target.epoch = 3;
+
+        assert!( is_slashable_attestation_data(&data_1, &data_2) );
+    }
+
+    #[test]
+    fn test_not_slashable_attestation_data() {
+        let mut data_1 = default_attestation_data();
+        let mut data_2 = default_attestation_data();
+        data_1.source.epoch = 0;
+        data_1.target.epoch = 4;
+        data_2.source.epoch = 4;
+        data_2.target.epoch = 5;
+        data_2.source.root = H256([1; 32]);
+        data_2.target.root = H256([1; 32]);
+
+        assert!( !is_slashable_attestation_data(&data_1, &data_2) );
     }
 
 }
