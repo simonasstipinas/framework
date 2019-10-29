a79783f37a18bdd909dc51d93ab12513ac679208
yoannna <o.smitaite@gmail.com>
2019-10-29 02:06:08 +0200

    get_active_validator_indices

--- helper-functions-2/src/beacon_state_accessors.rs
+++ helper-functions-2/src/beacon_state_accessors.rs
@@ -1,7 +1,8 @@
+use crate::predicates::is_active_validator;
 use ethereum_types::H256;
 use types::beacon_state::BeaconState;
 use types::config::Config;
-use types::primitives::{Epoch, Slot};
+use types::primitives::{Epoch, Slot, ValidatorIndex};
 
 const SLOTS_PER_HISTORICAL_ROOT: u64 = 2 ^ 13;
 const EPOCHS_PER_HISTORICAL_VECTOR: u64 = 2 ^ 16;
@@ -15,4 +16,18 @@ pub fn get_block_root_at_slot<C: Config>(state: BeaconState<C>, slot: Slot) -> H
 pub fn get_randao_mix<C: Config>(state: BeaconState<C>, epoch: Epoch) -> H256 {
     let index = (epoch % EPOCHS_PER_HISTORICAL_VECTOR) as usize;
     state.randao_mixes[index]
+}
+
+
+pub fn get_active_validator_indices<C: Config>(
+    state: BeaconState<C>,
+    epoch: Epoch,
+) -> Vec<ValidatorIndex> {
+    let mut validators = Vec::<ValidatorIndex>::new();
+    for (i, v) in state.validators.iter().enumerate() {
+        if is_active_validator(v, epoch) {
+            validators.push(i as ValidatorIndex)
+        }
+    }
+    validators
 }
