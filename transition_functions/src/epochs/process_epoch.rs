0958ed404621d557911b7b765e1c21a9f3996c6d
AtrixMU <matas@udris.lt>
2019-11-05 16:35:28 +0200

    add process_slashings and needed constants

--- transition_functions/src/epochs/process_epoch.rs
+++ transition_functions/src/epochs/process_epoch.rs
@@ -26,3 +26,18 @@ fn process_final_updates<T>(state: &mut BeaconState<T>) {
     //         if validator.activation_epoch == FAR_FUTURE_EPOCH:
     //             validator.activation_epoch = compute_activation_exit_epoch(get_current_epoch(state))
 }
+
+fn process_slashings<T>(state: &mut BeaconState<T>) {
+    let epoch = get_current_epoch(state)
+    //let total_balance = get_total_active_balance(state) //!missing helper function
+    let total_balance:u64 = 1 // placeholder
+    for (index, validator) in state.validators.iter().enumerate() {
+        if validator.slashed && epoch + EPOCHS_PER_SLASHING_VECTOR / 2 == validator.withdrawable_epoch {
+            let increment = EFFECTIVE_BALANCE_INCREMENT
+            let slashings_sum = state.slashings.iter().sum::<u64>();
+            let penalty_numerator = validator.effective_balance / increment * std::cmp::min(slashings_sum * 3, total_balance)
+            let penalty = penalty_numerator / total_balance * increment
+            //decrease_balance(state, ValidatorIndex(index), penalty) //! missing helper function
+        }
+    }
+}
--- types/src/consts.rs
+++ types/src/consts.rs
@@ -1,5 +1,19 @@
+use crate::primitives::*;
+
 pub const JUSTIFICATION_BITS_LENGTH: usize = 4;
 pub const SECONDS_PER_DAY: u64 = 86400;
 pub const DEPOSIT_CONTRACT_TREE_DEPTH: u64 = 32;
 pub type DepositContractTreeDepth = typenum::U32;
 pub type JustificationBitsLength = typenum::U4;
+
+// Gwei values
+pub const MIN_DEPOSIT_AMOUNT: Gwei = 1000000000;
+pub const MAX_EFFECTIVE_BALANCE: Gwei = 32000000000;
+pub const EJECTION_BALANCE: Gwei = 16000000000;
+pub const EFFECTIVE_BALANCE_INCREMENT: Gwei = 1000000000;
+
+// State list lengths
+pub const EPOCHS_PER_HISTORICAL_VECTOR:u64 = 0x1_0000;
+pub const EPOCHS_PER_SLASHINGS_VECTOR:u64 = 0x2000;
+pub const HISTORICAL_ROOTS_LIMIT:u64 = 0x100_0000;
+pub const VALIDATOR_REGISTRY_LIMIT:u64 = 0x100_0000_0000;
