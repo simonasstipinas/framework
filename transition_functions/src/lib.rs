4a48f0c0a230653551f49a52c8372099abf40b22
distlt-team <info@dist.lt>
2019-11-21 18:20:33 +0000

    Suppress lints in contributor code

--- transition_functions/src/lib.rs
+++ transition_functions/src/lib.rs
@@ -1,3 +1,8 @@
+// Lints are currently suppressed to prevent merge conflicts in case our contributors fix their code
+// on their own. These attributes should be removed in the future.
+#![allow(warnings)]
+#![allow(clippy::all)]
+
 pub mod attestations;
 pub mod blocks;
 pub mod epochs;
